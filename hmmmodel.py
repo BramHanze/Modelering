a4f9a0232fe41290a910737bb6b3d6db57894a38
Bram <145452823+BramHanze@users.noreply.github.com>
2024-11-25 20:29:28 +0100

    Add files via upload

--- hmmmodel.py
+++ hmmmodel.py
@@ -3,17 +3,14 @@ import numpy as np
 class HiddenMarkovModel:
     def __init__(self, n_components, n_features):
         """
-        Initialiseer een Hidden Markov Model.
-        
-        Parameters:
-        - n_components: int, aantal toestanden.
-        - n_features: int, aantal verschillende emissies.
+        n_components: int, aantal tafels.
+        n_features: int, aantal verschillende kleuren.
         """
         self.n_components = n_components
         self.n_features = n_features
-        self.startprob_ = np.zeros(n_components)  # Begintoestandsverdeling
-        self.transmat_ = np.zeros((n_components, n_components))  # Overgangswaarschijnlijkheden
-        self.emissionprob_ = np.zeros((n_components, n_features))  # Emissiekansen
+        self.startprob_ = np.zeros(n_components)
+        self.transmat_ = np.zeros((n_components, n_components))
+        self.emissionprob_ = np.zeros((n_components, n_features))
 
     def __str__(self):
         return (f"HiddenMarkovModel(n_components={self.n_components}, "
@@ -23,29 +20,16 @@ class HiddenMarkovModel:
         return self.__str__()
 
     def sample(self, n_samples):
-        """
-        Genereer een sequentie van toestanden en waarnemingen.
-
-        Parameters:
-        - n_samples: int, aantal te genereren waarnemingen.
-
-        Returns:
-        - emissions: lijst van gegenereerde waarnemingen.
-        - states: lijst van gegenereerde toestanden.
-        """
         states = []
         emissions = []
 
-        # Begin met de initiële toestand
         current_state = np.random.choice(self.n_components, p=self.startprob_)
         states.append(current_state)
 
-        # Voeg de eerste emissie toe
         emissions.append(
             np.random.choice(self.n_features, p=self.emissionprob_[current_state])
         )
 
-        # Genereer de rest
         for _ in range(1, n_samples):
             current_state = np.random.choice(self.n_components, p=self.transmat_[current_state])
             states.append(current_state)
